26bfe40ea9a0035c9c03631f3b07b1858dcc1497
harrison <harrison.vanderbyl@gmail.com>
2023-03-15 10:00:40 +1100

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+# install libtorch
+wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
+unzip libtorch-shared-with-deps-latest.zip
+rm libtorch-shared-with-deps-latest.zip
